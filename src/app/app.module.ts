5da88db1199e141511ab8356684687e19213e44c
d-adap <dileepadapala@gmail.com>
2017-10-18 00:18:46 +0100

    added multiple components, services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
+import { HeroDetailComponent } from './hero-detail.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
 ],
   imports: [
     BrowserModule,
