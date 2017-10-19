a5cea4049caa998c7b0436d1b99b7cf73b1fe0d9
Dileep Adapala <gbadapad@uk.sony.dev>
2017-10-19 16:01:12 +0100

    adding intermediate changes- !! Code Broken !!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { AppRoutingModule } from './app-routing.module';
-import { HeroService } from './heroes/hero.service';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './heroDetails/hero-detail.component';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroService } from './heroes/hero.service';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './heroDetails/hero-detail.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +25,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [HeroService],
--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -1,23 +1,34 @@
-import { HEROES } from './mock-heroes';
-import { Hero } from './hero';
-
+import { Headers, Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/toPromise';
+
+import { Hero } from './hero';
 
 @Injectable()
 export class HeroService {
-
-    getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
-    }
-    getHeroesSlowly(): Promise<Hero[]> {
+  private heroesUrl = 'api/heroes';
+  constructor(private http: Http) { }
+  getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesUrl).toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
+        // return Promise.resolve(HEROES);
+  }
+  getHeroesSlowly(): Promise<Hero[]> {
         return new Promise(resolve => {
           // Simulate server latency with 2 second delay
           setTimeout(() => resolve(this.getHeroes()), 20000);
         });
-      }
-      getHero(id: number): Promise<Hero> {
-          console.log(id);
-        return this.getHeroes()
-        .then(heroes => heroes.find(hero => hero.id === id));
-      }
+   }
+  getHero(id: number): Promise<Hero> {
+      console.log(id);
+      const url = '${this.heroesUrl}/${id}';
+      return this.http.get(url).toPromise()
+      .then(response => response.json().data as Hero)
+      .catch(this.handleError);
+  }
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
 }
